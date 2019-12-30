308851931eb013934a6946890d40239455f58152
Dandan Meng <mengdandanno1@163.com>
2019-12-30 08:43:43 +0800

    Lesson 7-14: move the motion with code

--- motionlayout-start/app/src/main/java/com/google/samples/motionlayoutcodelab/Step8Activity.kt
+++ motionlayout-start/app/src/main/java/com/google/samples/motionlayoutcodelab/Step8Activity.kt
@@ -17,6 +17,8 @@ package com.google.samples.motionlayoutcodelab
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.google.android.material.appbar.AppBarLayout
 
 class Step8Activity : AppCompatActivity() {
 
@@ -28,7 +30,16 @@ class Step8Activity : AppCompatActivity() {
     }
 
     private fun coordinateMotion() {
-        // TODO: set progress of MotionLayout based on an AppBarLayout.OnOffsetChangedListener
+        // set progress of MotionLayout based on an AppBarLayout.OnOffsetChangedListener
 
+        val appBarLayout: AppBarLayout = findViewById(R.id.appbar_layout)
+        val motionLayout: MotionLayout = findViewById(R.id.motion_layout)
+
+        val listener = AppBarLayout.OnOffsetChangedListener { unused, verticalOffset ->
+            val seekPosition = -verticalOffset / appBarLayout.totalScrollRange.toFloat()
+            motionLayout.progress = seekPosition
+        }
+
+        appBarLayout.addOnOffsetChangedListener(listener)
     }
 }
